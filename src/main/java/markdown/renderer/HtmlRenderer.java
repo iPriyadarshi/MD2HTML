a24918891b0a781ac58b3253f7be4dfb0ed6bee4
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 15:56:50 +0530

    Implemented HtmlRenderer to render HTML using helper class HtmlBuilder

--- src/main/java/markdown/renderer/HtmlRenderer.java
+++ src/main/java/markdown/renderer/HtmlRenderer.java
@@ -1,4 +1,59 @@
 package markdown.renderer;
 
-public class HtmlRenderer {
+import markdown.parser.nodes.*;
+
+public class HtmlRenderer implements Renderer {
+
+    @Override
+    public String render(Node node) {
+
+        HtmlBuilder builder = new HtmlBuilder();
+
+        renderNode(node, builder);
+
+        return builder.getHtml();
+    }
+
+    private void renderNode(Node node, HtmlBuilder builder) {
+
+        switch (node) {
+
+            case DocumentNode document -> {
+
+                for (Node child : document.getChildren()) {
+                    renderNode(child, builder);
+                }
+            }
+
+            case HeadingNode heading -> {
+
+                String tag = "h" + heading.getLevel();
+
+                builder.openTag(tag);
+
+                for (Node child : heading.getChildren()) {
+                    renderNode(child, builder);
+                }
+
+                builder.closeTag(tag);
+                builder.newLine();
+            }
+
+            case ParagraphNode paragraph -> {
+
+                builder.openTag("p");
+
+                for (Node child : paragraph.getChildren()) {
+                    renderNode(child, builder);
+                }
+
+                builder.closeTag("p");
+                builder.newLine();
+            }
+
+            case TextNode textNode -> builder.addText(textNode.getText());
+
+            default -> throw new RuntimeException("Unknown node type: " + node.getClass());
+        }
+    }
 }
