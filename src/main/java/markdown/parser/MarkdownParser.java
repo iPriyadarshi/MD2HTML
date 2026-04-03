6f2867a637908b4b8078c99f5fcbd79d99dab5a4
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 15:48:24 +0530

    Implemented parser for Markdown

--- src/main/java/markdown/parser/MarkdownParser.java
+++ src/main/java/markdown/parser/MarkdownParser.java
@@ -1,4 +1,145 @@
 package markdown.parser;
 
-public class MarkdownParser {
+import markdown.lexer.Token;
+import markdown.lexer.TokenType;
+import markdown.parser.nodes.*;
+
+import java.util.List;
+
+public class MarkdownParser implements Parser {
+
+    private List<Token> tokens;
+    private int position;
+
+    @Override
+    public DocumentNode parse(List<Token> tokens) {
+
+        this.tokens = tokens;
+        this.position = 0;
+
+        DocumentNode document = new DocumentNode();
+
+        while (!isAtEnd()) {
+
+            Node block = parseBlock();
+
+            if (block != null) {
+                document.addChild(block);
+            }
+        }
+
+        return document;
+    }
+
+    private Node parseBlock() {
+
+        if (check(TokenType.HASH)) {
+            return parseHeading();
+        }
+
+        if (check(TokenType.TEXT)) {
+            return parseParagraph();
+        }
+
+        // skip unexpected tokens
+        advance();
+
+        return null;
+    }
+
+    private HeadingNode parseHeading() {
+
+        int level = 0;
+
+        // count number of # symbols
+        while (match(TokenType.HASH)) {
+            level++;
+        }
+
+        HeadingNode heading = new HeadingNode(level);
+
+        // heading text
+        while (!check(TokenType.NEWLINE) && !isAtEnd()) {
+
+            Token token = advance();
+
+            if (token.getType() == TokenType.TEXT) {
+
+                heading.addChild(new TextNode(token.getValue()));
+            }
+        }
+
+        consumeNewline();
+
+        return heading;
+    }
+
+    private ParagraphNode parseParagraph() {
+
+        ParagraphNode paragraph = new ParagraphNode();
+
+        while (!check(TokenType.NEWLINE) && !isAtEnd()) {
+
+            Token token = advance();
+
+            if (token.getType() == TokenType.TEXT) {
+
+                paragraph.addChild(new TextNode(token.getValue()));
+            }
+        }
+
+        consumeNewline();
+
+        return paragraph;
+    }
+
+    // helper methods
+
+    private boolean match(TokenType type) {
+
+        if (check(type)) {
+            advance();
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+
+        if (isAtEnd()) return false;
+
+        return peek().getType() == type;
+    }
+
+    private Token advance() {
+
+        if (!isAtEnd()) {
+            position++;
+        }
+
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+
+        return peek().getType() == TokenType.EOF;
+    }
+
+    private Token peek() {
+
+        return tokens.get(position);
+    }
+
+    private Token previous() {
+
+        return tokens.get(position - 1);
+    }
+
+    private void consumeNewline() {
+
+        if (check(TokenType.NEWLINE)) {
+            advance();
+        }
+    }
 }
