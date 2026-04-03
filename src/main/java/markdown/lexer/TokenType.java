bf7eb4302946af353a9277b31658c04366a9e5dc
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 15:08:27 +0530

    Added Token types

--- src/main/java/markdown/lexer/TokenType.java
+++ src/main/java/markdown/lexer/TokenType.java
@@ -2,4 +2,21 @@ package markdown.lexer;
 
 public enum TokenType {
 
+    // structure
+    HASH,          // #
+    DASH,          // -
+    NEWLINE,
+
+    // text
+    TEXT,
+
+    // inline formatting
+    STAR,          // *
+    BACKTICK,      // `
+    OPEN_BRACKET,  // [
+    CLOSE_BRACKET, // ]
+    OPEN_PAREN,    // (
+    CLOSE_PAREN,   // )
+
+    EOF
 }
