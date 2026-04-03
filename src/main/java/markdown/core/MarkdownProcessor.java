2efaf3e5f4c8b69418e7d1e3ab123576790b4a62
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 18:34:52 +0530

    Added MarkdownProcessor as a facade over the whole system

--- src/main/java/markdown/core/MarkdownProcessor.java
+++ src/main/java/markdown/core/MarkdownProcessor.java
@@ -1,5 +1,38 @@
 package markdown.core;
 
+import markdown.lexer.Lexer;
+import markdown.lexer.MarkdownLexer;
+import markdown.lexer.Token;
+import markdown.parser.MarkdownParser;
+import markdown.parser.Parser;
+import markdown.parser.nodes.DocumentNode;
+import markdown.renderer.HtmlRenderer;
+import markdown.renderer.Renderer;
+
+import java.util.List;
+
 public class MarkdownProcessor {
-    
+
+    private final Lexer lexer;
+    private final Parser parser;
+    private final Renderer renderer;
+
+    public MarkdownProcessor() {
+
+        this.lexer = new MarkdownLexer();
+        this.parser = new MarkdownParser();
+        this.renderer = new HtmlRenderer();
+    }
+
+    public String process(String markdown) {
+
+        // Step 1: tokenize
+        List<Token> tokens = lexer.tokenize(markdown);
+
+        // Step 2: parse tokens into AST
+        DocumentNode document = parser.parse(tokens);
+
+        // Step 3: render AST to HTML
+        return renderer.render(document);
+    }
 }
