f8752b65ceded88060c9d3474ab9d06d24ece892
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 15:09:53 +0530

    Added Token class

--- src/main/java/markdown/lexer/Token.java
+++ src/main/java/markdown/lexer/Token.java
@@ -1,5 +1,31 @@
 package markdown.lexer;
 
 public class Token {
-    
+
+    private final TokenType type;
+    private final String value;
+    private final int position;
+
+    public Token(TokenType type, String value, int position) {
+        this.type = type;
+        this.value = value;
+        this.position = position;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "type=" + type + ", value='" + value + '\'' + ", position=" + position + '}';
+    }
 }
