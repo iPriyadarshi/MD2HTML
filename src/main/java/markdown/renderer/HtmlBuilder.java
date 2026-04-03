9a84ebe9ce32ee518b0468ff401f6a5416e0d0e1
iPriyadarshi <priyadarshi1505@gmail.com>
2026-04-03 15:55:24 +0530

    Add HtmlBuilder to create HTML from tags and texts

--- src/main/java/markdown/renderer/HtmlBuilder.java
+++ src/main/java/markdown/renderer/HtmlBuilder.java
@@ -1,4 +1,26 @@
 package markdown.renderer;
 
 public class HtmlBuilder {
+
+    private final StringBuilder html = new StringBuilder();
+
+    public void openTag(String tag) {
+        html.append("<").append(tag).append(">");
+    }
+
+    public void closeTag(String tag) {
+        html.append("</").append(tag).append(">");
+    }
+
+    public void addText(String text) {
+        html.append(text);
+    }
+
+    public void newLine() {
+        html.append("\n");
+    }
+
+    public String getHtml() {
+        return html.toString();
+    }
 }
